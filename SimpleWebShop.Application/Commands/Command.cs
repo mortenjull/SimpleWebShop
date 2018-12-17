da8a079005eb492ba5c318bb9891ff18f6b9399b
mortenjull <mortenjull@hotmail.com>
2018-12-17 10:15:45 +0100

    asd

--- SimpleWebShop.Application/Commands/Command.cs
+++ SimpleWebShop.Application/Commands/Command.cs
@@ -4,5 +4,9 @@ using System.Text;
 
 namespace SimpleWebShop.Application.Commands
 {
+    /// <summary>
+    /// Funktions as an ankor for locading commands 
+    /// in assembly from startup inside SimpleWebShop.
+    /// </summary>
     public class Command { }
 }
---
+++ SimpleWebShop.Application/Commands/Search/NumberInRangeCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SimpleWebShop.Application.Commands.Search
+{
+    public class NumberInRangeCommand : IRequest<bool>
+    {
+        public NumberInRangeCommand(int number)
+        {
+            this.Number = number;
+        }
+
+        public int Number { get; set; }
+    }
+
+    public class NumberInRangeCommandHandler : IRequestHandler<NumberInRangeCommand, bool>
+    {
+        public async Task<bool> Handle(NumberInRangeCommand request, CancellationToken cancellationToken)
+        {
+            int min = 0;
+            int max = 100;
+            int number = request.Number;
+
+            if (number >= min && number <= max)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
