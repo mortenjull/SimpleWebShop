cabced254da1ddebd0e93fae99bea499ce057298
Whiitetesla <whiite.h.l@gmail.com>
2018-12-20 11:29:15 +0100

    Added a new specflow

--- SimpleWebShop.SpecFlow/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SimpleWebShop.SpecFlow
-{
-    public class Class1
-    {
-    }
-}
---
+++ SimpleWebShop.Specflow/NumberInRangeSteps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace SimpleWebShop.Specflow
+{
+    [Binding]
+    public class NumberInRangeSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.Equal(120,p0);
+        }
+    }
+}
