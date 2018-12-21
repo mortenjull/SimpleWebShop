43d310b8c353d38c2dcf351caca72432fec4bb6a
Whiitetesla <whiite.h.l@gmail.com>
2018-12-21 08:44:47 +0100

    added xunit runner as adapter to run specflow

--- SimpleWebShop.Specflow/NumberInRangeSteps.cs
+++ SimpleWebShop.Specflow/NumberInRangeSteps.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using TechTalk.SpecFlow;
 using Xunit;
 
@@ -7,22 +8,28 @@ namespace SimpleWebShop.Specflow
     [Binding]
     public class NumberInRangeSteps
     {
+        List<int> numbers = new List<int>();
+        int result = 0; 
+
         [Given(@"I have entered (.*) into the calculator")]
         public void GivenIHaveEnteredIntoTheCalculator(int p0)
         {
-            
+            numbers.Add(p0);
         }
         
         [When(@"I press add")]
         public void WhenIPressAdd()
         {
-            
+            foreach(int number in numbers)
+            {
+                result += number;
+            }
         }
         
         [Then(@"the result should be (.*) on the screen")]
         public void ThenTheResultShouldBeOnTheScreen(int p0)
         {
-            Assert.Equal(120,p0);
+            Assert.Equal(result, p0);
         }
     }
 }
--- SimpleWebShop.UnitTest/UnitTest1.cs
+++ SimpleWebShop.UnitTest/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace SimpleWebShop.UnitTest
         [Fact]
         public void Test1()
         {
-
+            Assert.True(true);
         }
     }
 }
