d6e572e1362c39fef69c24e1fdd8cfd0994f9822
Tim Knudsen <>
2018-12-28 09:01:21 +0100

    Added spec test for testing the price range slider in search.

--- SimpleWebShop.Cypress/cypress/integration/shop_search_spec.js
+++ SimpleWebShop.Cypress/cypress/integration/shop_search_spec.js
@@ -29,15 +29,106 @@ describe('Shop Search functionality.', () => {
         });
     });
 
-    it('Returns less elements when applying min price filter.', () => {
+    it('Returns products with higher price when applying min price filter.', () => {
+        // Get the min price slider.
         cy.get('[data-cy=search-min-price-slider]').as('search-min-price-slider');
         
-        let offset = cy.get('@search-min-price-slider').invoke("offset");
-        console.log(offset);
-        cy.get('@search-min-price-slider')
-            .trigger('mousedown', { which: 1, pageX: offset.left, pageY: offset.top })
-            .trigger('mousemove', { which: 1, pageX:  offset.left + 10, pageY:  offset.left + 10 })
-            .trigger('mouseup')
+        // Get the offset of the min price rangler slider.
+        cy.get('@search-min-price-slider').invoke('offset').then(minOffset => {
+            // Move the min price range slider.
+            cy.get('@search-min-price-slider')
+                .trigger('mousedown', { which: 1, pageX: minOffset.left, pageY: minOffset.top })
+                .trigger('mousemove', { which: 1, pageX:  minOffset.left + 50, pageY:  minOffset.top })
+                .trigger('mouseup');
+
+            // Get the selected min and max price.
+            cy.get('[data-cy=search-min]').invoke('val').then($min => {
+                cy.get('[data-cy=search-max]').invoke('val').then($max => {
+                    // Convert string values to float.
+                    $min = parseFloat($min);
+                    $max = parseFloat($max);
+
+                    // Apply default filter options.
+                    cy.get('[data-cy=search-submit').click();
+                    
+                    // Check if each product has a price below the choosen min price.
+                    cy.get('[data-cy=search-product-price]').invoke('val').should($price => {
+                        expect(parseFloat($price)).to.be.within($min, $max);
+                    });
+                });
+            });
+        });
+    });
+
+    it('Returns products with lower price when applying max price filter.', () => {
+        // Get the max price slider.
+        cy.get('[data-cy=search-max-price-slider]').as('search-max-price-slider');
+        
+        // Get the offset of the min price rangler slider.
+        cy.get('@search-max-price-slider').invoke('offset').then(maxOffset => {
+            // Move the max price range slider.
+            cy.get('@search-max-price-slider')
+                .trigger('mousedown', { which: 1, pageX: maxOffset.left, pageY: maxOffset.top })
+                .trigger('mousemove', { which: 1, pageX:  maxOffset.left - 50, pageY:  maxOffset.top })
+                .trigger('mouseup');
+
+            // Get the selected min and max price.
+            cy.get('[data-cy=search-min]').invoke('val').then($min => {
+                cy.get('[data-cy=search-max]').invoke('val').then($max => {
+                    // Convert string values to float.
+                    $min = parseFloat($min);
+                    $max = parseFloat($max);
+
+                    // Apply default filter options.
+                    cy.get('[data-cy=search-submit').click();
+                    
+                    // Check if each product has a price below the choosen max price.
+                    cy.get('[data-cy=search-product-price]').invoke('val').should($price => {
+                        expect(parseFloat($price)).to.be.within($min, $max);
+                    });
+                });
+            });
+        });
+    });
+
+    it('Returns with price between min and max price filter.', () => {
+        // Get the max price slider.
+        cy.get('[data-cy=search-min-price-slider]').as('search-min-price-slider');
+        cy.get('[data-cy=search-max-price-slider]').as('search-max-price-slider');
+        
+        // Get the offset of the min price rangler slider.
+        cy.get('@search-max-price-slider').invoke('offset').then(maxOffset => {
+            cy.get('@search-min-price-slider').invoke('offset').then(minOffset => {
+                // Move the max price range slider.
+                cy.get('@search-max-price-slider')
+                    .trigger('mousedown', { which: 1, pageX: maxOffset.left, pageY: maxOffset.top })
+                    .trigger('mousemove', { which: 1, pageX:  maxOffset.left - 50, pageY:  maxOffset.top })
+                    .trigger('mouseup');
+
+                // Move the min price range slider.
+                cy.get('@search-min-price-slider')
+                    .trigger('mousedown', { which: 1, pageX: minOffset.left, pageY: minOffset.top })
+                    .trigger('mousemove', { which: 1, pageX:  minOffset.left + 50, pageY:  minOffset.top })
+                    .trigger('mouseup');
+
+                // Get the selected min and max price.
+                cy.get('[data-cy=search-min]').invoke('val').then($min => {
+                    cy.get('[data-cy=search-max]').invoke('val').then($max => {
+                        // Convert string values to float.
+                        $min = parseFloat($min);
+                        $max = parseFloat($max);
+
+                        // Apply default filter options.
+                        cy.get('[data-cy=search-submit').click();
+                        
+                        // Check if each product has a price below the choosen max price.
+                        cy.get('[data-cy=search-product-price]').invoke('val').should($price => {
+                            expect(parseFloat($price)).to.be.within($min, $max);
+                        });
+                    });
+                });
+            });
+        });
     });
 
 });
