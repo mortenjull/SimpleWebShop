3c2b685e0a831aa98daf69ae50ba7f64425a6b61
Whiitetesla <whiite.h.l@gmail.com>
2019-01-02 14:34:12 +0100

    added unit of work mock

---
+++ SimpleWebShop.UnitTest/Fake/FakeSearchCommandRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleWebShop.UnitTest.Fake
+{
+    class FakeSearchCommandRepository
+    {
+    }
+}
--- SimpleWebShop.UnitTest/SearchCommandTests.cs
+++ SimpleWebShop.UnitTest/SearchCommandTests.cs
@@ -1,13 +1,21 @@
-﻿using SimpleWebShop.UnitTest.DataGenerators;
+﻿using SimpleWebShop.Application.Commands.Search;
+using SimpleWebShop.UnitTest.DataGenerators;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using Moq;
 using Xunit;
+using SimpleWebShop.Domain.UnitOfWorks;
+using SimpleWebShop.Domain.UnitOfWorks.Repositories;
+using SimpleWebShop.Domain.Entities;
+using SimpleWebShop.UnitTest.Fake;
 
 namespace SimpleWebShop.UnitTest
 {
     public class SearchCommandTests
     {
+
         [Fact]
         public void TestCase1_Fact_True()
         {
@@ -18,9 +26,22 @@ namespace SimpleWebShop.UnitTest
         [ClassData(typeof(TestSearchDataGenerator))]
         public void TestCase2_Theory_True(double minPrice, double maxPrice, List<int> colors)
         {
-            Assert.True(true);
-            Assert.Equal(0.0, minPrice);
-            Assert.Equal(10000.0, maxPrice);
+            var searchProduct = new SearchProductCommand(minPrice, maxPrice, colors);
+            CancellationToken token = new CancellationToken();
+
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            Mock<IRepository> mockRepository = new Mock<IRepository>();
+
+            mockRepository.Setup(x => x.All<Color>(token)).Returns(new FakeSearchCommandRepository().GetAll);
+            mockUnitOfWork.Setup(x => x.Repository).Returns(mockRepository.Object);
+
+            var searchProductHandler = new SearchProductCommandHandler(mockUnitOfWork.Object);
+
+            var list = searchProductHandler.Handle(searchProduct, token).Result;
+
+            Assert.NotEmpty(list);
+
+            Assert.True(false);
         }
     }
 }
